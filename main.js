6ee5e92161b2faedfd5d19084d50954728df1650
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-07-15 00:21:58 -0700

    added skrollr for better look

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
+  var s = skrollr.init();
+
   $("a").mouseover(function() {
     $(this).addClass("animated pulse")
+  }).mouseout(function() {
+    $(this).removeClass("animated pulse")
   });
 
+  $("#name").mouseover(function () {
+    $(this).addClass("animated rubberBand")
+  }).mouseout(function () {
+    $(this).removeClass("animated rubberBand")
+  });
 });
