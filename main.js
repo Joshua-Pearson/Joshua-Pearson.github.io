ff44e0e1ceb47d50db47296d8bf621fdc46225eb
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-07-15 02:51:25 -0700

    still fussing with a bunch of stuff, not sure i like the format

--- main.js
+++ main.js
@@ -3,14 +3,30 @@ $(document).ready(function() {
   var s = skrollr.init();
 
   $("a").mouseover(function() {
-    $(this).addClass("animated pulse")
+    $(this).addClass("animated pulse");
   }).mouseout(function() {
-    $(this).removeClass("animated pulse")
+    $(this).removeClass("animated pulse");
   });
 
-  $("#name").mouseover(function () {
-    $(this).addClass("animated rubberBand")
+  $("#name").mouseover(function() {
+    $(this).addClass("animated rubberBand");
   }).mouseout(function () {
-    $(this).removeClass("animated rubberBand")
+    $(this).removeClass("animated rubberBand");
   });
+
+  // $(window).scroll(function() {
+  //   $("#top nav .contacts_buttons").addClass("fa-spin");
+  //   setTimeout(function() {
+  //     $("#top nav .contacts_buttons").removeClass("fa-spin");
+  //   }, 2000);
+  // });
+
+  $(window).scroll(function() {
+    $("#social-links a").addClass("fa-spin");
+    setTimeout(function() {
+      $("#social-links a").removeClass("fa-spin");
+    }, 2000);
+  });
+
+
 });
