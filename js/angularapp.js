1ff9fd1ca0d50e45434676fa2dc7f5b03471bd9f
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-08-29 01:53:07 -0700

    fussing with index2

--- js/angularapp.js
+++ js/angularapp.js
@@ -1,5 +1,22 @@
 (function() {
 
   var myApp = angular.module("myApp", []);
-  
+
+  myApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+      .state('home', {
+          url: '/home',
+          templateUrl: 'partial-home.html'
+      })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+      .state('about', {
+            // we'll get to this in a bit       
+    });
+  });
 })();
