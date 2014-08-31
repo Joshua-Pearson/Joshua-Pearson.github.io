94c417d72c17176222a5be61ea01119b8f9c0520
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-08-30 23:20:02 -0700

    working with angular states

--- js/angularapp.js
+++ js/angularapp.js
@@ -11,12 +11,28 @@
         // HOME STATES AND NESTED VIEWS ========================================
       .state('home', {
           url: '/home',
-          templateUrl: 'partial-home.html'
+          templateUrl: 'partials/home.html'
       })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
       .state('about', {
-            // we'll get to this in a bit       
+          url: '/about',
+          templateUrl: 'partials/about.html'
+      })
+
+      .state('projects' {
+        url: '/projects',
+        templateUrl: 'partials/projects.hmtl'
+      })
+
+      .state('blog', {
+        url: '/blog',
+        templateUrl: 'partials/blog.html'
+      })
+
+      .state('contact'), {
+        url: '/contact',
+        templateUrl: 'partials/contact.html'
     });
   });
 })();
